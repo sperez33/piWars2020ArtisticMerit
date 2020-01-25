cc8980bf62e67e90ea6c78096b3fe31def3795c8
Steph Perez <zombie.licious4brains@gmail.com>
2020-01-25 11:49:31 -0700

    First commit.

---
+++ musicalNote_v1.py
@@ -0,0 +1,12 @@
+#Steph Version 1 Artistic Merit, no lights only music
+#Programmer: Steph Perez
+#email: sperez33@cnm.edu or steph.perez1994@gmail.com
+# purpose: have the option to play music while competing in the various challenges at Pi Wars 2020
+# we would like to eventually add in code to control LED lights as well.
+# we hope to get additional points for artistic merit.
+
+import csv
+import pygame
+from pygame import *
+import sys
+import os
